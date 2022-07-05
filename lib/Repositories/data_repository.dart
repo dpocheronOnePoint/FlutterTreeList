af135752772beca11416739d3363f865caa47505
your_username <your_email@gmail.com>
2022-07-05 09:49:03 +0200

    Add function to delete all trees in database

--- lib/Repositories/data_repository.dart
+++ lib/Repositories/data_repository.dart
@@ -2,14 +2,13 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tree_list/models/tree.dart';
 import 'package:flutter_tree_list/services/api_service.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
 
 class DataRepository with ChangeNotifier {
   final APIService apiService = APIService();
   final List<Tree> _treeList = [];
   int startIndex = 0;
-
-  // bool wsAlreadyInProgress = false;
 
   // Getters
   List<Tree> get treeList => _treeList;
@@ -24,28 +23,26 @@ class DataRepository with ChangeNotifier {
       if (localTrees.isEmpty) {
         List<Tree> trees = await apiService.getTreeList(startIndex);
 
+        await removeAllTreeInDatabase();
+
         trees.forEach((tree) async {
           await addTreeInDatabase(tree);
         });
 
         _treeList.addAll(trees);
       } else {
-        print('Yes j\'ai de la data');
         _treeList.addAll(localTrees);
       }
 
       notifyListeners();
-      // wsAlreadyInProgress = false;
     } on Response catch (response) {
       print("Error: ${response.statusCode}");
-      // wsAlreadyInProgress = false;
       rethrow;
     }
   }
 
   Future<List<Tree>> getTreesFromDd() async {
-    final db = database;
-    final List<Map<String, dynamic>> maps = await db.query('tree');
+    final List<Map<String, dynamic>> maps = await database.query('tree');
 
     return List.generate(maps.length, (index) {
       return Tree(id: maps[index]['id'], name: maps[index]['name'] ?? '');
@@ -53,11 +50,9 @@ class DataRepository with ChangeNotifier {
   }
 
   Future<void> addTreeInDatabase(Tree tree) async {
-    final db = database;
-
     final Tree treeToAdd = Tree(id: tree.id, name: tree.name ?? '');
 
-    db.insert("tree", treeToAdd.toJson(),
+    database.insert("tree", treeToAdd.toJson(),
         conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
@@ -65,9 +60,9 @@ class DataRepository with ChangeNotifier {
 // Functio to remove item from database
 /////////////////////////////////////////
 
-  // Future<void> removeTreeInDatabase(Tree treeToRemove) async {
-  //   final db = await database;
-
-  //   db.delete("tree", where: 'id = ?', whereArgs: [treeToRemove.id]);
-  // }
+  Future<void> removeAllTreeInDatabase() async {
+    await database.delete(
+      "tree", /*where: 'id = ?', whereArgs: [treeToRemove.id]*/
+    );
+  }
 }
--- lib/ui/screens/loading_screen.dart
+++ lib/ui/screens/loading_screen.dart
@@ -23,57 +23,56 @@ class _LoadingScreenState extends State<LoadingScreen> {
     final dataProvider = Provider.of<DataRepository>(context, listen: false);
     await dataProvider.getTrees();
 
-    // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-    //   return const TreeListScreen();
-    // }));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+      return const TreeListScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   color: Colors.lightGreen,
-    //   child:
-    //       Column(mainAxisAlignment: MainAxisAlignment.center, children: const [
-    //     SpinKitFadingCircle(
-    //       color: Colors.white,
-    //       size: 30,
-    //     )
-    //   ]),
-    // );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Connection Notifier Demo'),
-      ),
-
-      /// If you want to toggle some widgets based on connection state
-      body: ConnectionNotifierToggler(
-        onConnectionStatusChanged: (connected) {
-          /// that means it is still in the initialization phase.
-          if (connected == null) return;
-          print(connected);
-        },
-        connected: Center(
-          key: UniqueKey(),
-          child: const Text(
-            'Connected',
-            style: TextStyle(
-              color: Colors.green,
-              fontSize: 48,
-            ),
-          ),
-        ),
-        disconnected: Center(
-          key: UniqueKey(),
-          child: const Text(
-            'Disconnected',
-            style: TextStyle(
-              color: Colors.red,
-              fontSize: 48,
-            ),
-          ),
-        ),
-      ),
+    return Container(
+      color: Colors.lightGreen,
+      child:
+          Column(mainAxisAlignment: MainAxisAlignment.center, children: const [
+        SpinKitFadingCircle(
+          color: Colors.white,
+          size: 30,
+        )
+      ]),
     );
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Connection Notifier Demo'),
+    //   ),
+
+    //   /// If you want to toggle some widgets based on connection state
+    //   body: ConnectionNotifierToggler(
+    //     onConnectionStatusChanged: (connected) {
+    //       /// that means it is still in the initialization phase.
+    //       if (connected == null) return;
+    //     },
+    //     connected: Center(
+    //       key: UniqueKey(),
+    //       child: const Text(
+    //         'Connected',
+    //         style: TextStyle(
+    //           color: Colors.green,
+    //           fontSize: 48,
+    //         ),
+    //       ),
+    //     ),
+    //     disconnected: Center(
+    //       key: UniqueKey(),
+    //       child: const Text(
+    //         'Disconnected',
+    //         style: TextStyle(
+    //           color: Colors.red,
+    //           fontSize: 48,
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
